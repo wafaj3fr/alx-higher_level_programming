caf594e9dfd0f5a249290623404db4350cb15037
wafaa <wafaagaafar@gmail.com>
2023-11-28 05:12:24 -0800

    pep

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z') + 1):
-    if chr(i) == 'e' or chr(i) == 'q':
+    if chr(i)=='e' or chr(i)=='q':
         continue
-    print("{}".format(chr(i)), end= "")
+    print("{}".format(chr(i)), end="")
--- 0x01-python-if_else_loops_functions/4-print_hexa.py
+++ 0x01-python-if_else_loops_functions/4-print_hexa.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for i in range(99):
     x = "{:x}".format(i)
-    print("{} = 0x{}".format(i,x))
+    print("{} = 0x{}".format(i, x))
