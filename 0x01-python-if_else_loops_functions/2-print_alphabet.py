a385db8ce4d5b5651a349b262cb7665b637e3fc7
wafaa <wafaagaafar@gmail.com>
2023-11-28 03:02:58 -0800

    alphabet

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,5 +1,3 @@
 #!/usr/bin/python3
-alpha = ""
-for i in range(97, 123):
-    alpha += "{}".format(chr(i))
-print(alpha)
+for i in range (ord('a'), ord('z') + 1):
+	print("{:s}".format(chr(i)), end= "")
