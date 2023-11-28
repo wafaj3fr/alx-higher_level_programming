f173cdc3bd29d0d6a2c193994389b6ad3e39303c
wafaa <wafaagaafar@gmail.com>
2023-11-27 21:29:26 -0800

    edit

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 import sys
-line = "and that piece of art is useful - Dora Korpar, 2015-10-19"
+line = "and that piece of art is useful - Dora Korpar, 2015-10-19\n"
 sys.stderr.write(line)
 sys.exit(1)
