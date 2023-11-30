cbeaf62d28b90f0c2ddfd59f790cd95fee8e1e20
wafaa <wafaagaafar@gmail.com>
2023-11-30 03:39:25 -0800

    3

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
-import sys
-
-def sum_arguments():
-    arguments = sys.argv[1:]
-    result = sum(int(arg) for arg in arguments)
+if __name__ == "__main__":
+    import sys
+    result = 0
+    for arg in sys.argv:
+        if arg != sys.argv[0]:
+            result += int(arg)
     print(result)
-
-sum_arguments()
