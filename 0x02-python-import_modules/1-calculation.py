94fdddd237c380355db07fcb77f7ba04912348af
wafaa <wafaagaafar@gmail.com>
2023-11-30 03:29:14 -0800

    1

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a, b, calculator.add(a, b)))
-    print("{} = {} = {}".format(a, b, calculator.sub(a, b)))
-    print("{} * {} = {}".format(a, b, calculator.mul(a, b)))
-    print("{} / {} = {}".format(a, b, calculator.div(a, b)))
+    print("{} + {} = {}".format(a, b, add(a, b)))
+    print("{} = {} = {}".format(a, b, sub(a, b)))
+    print("{} * {} = {}".format(a, b, mul(a, b)))
+    print("{} / {} = {}".format(a, b, div(a, b)))
