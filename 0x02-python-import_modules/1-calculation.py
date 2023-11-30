228f9c037e3d76d75841b580d0d67aee18f29aa7
wafaa <wafaagaafar@gmail.com>
2023-11-30 03:23:40 -0800

    edits

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
-import calculator_1
 
-a = 10
-b = 5
+if __name__ == "__main__":
+    """Print the summation, subtraction, multiplication and division of 10 and 5."""
+    import calculator_1
 
-print(f"{a} + {b} = {calculator_1.add(a, b)}")
-print(f"{a} - {b} = {calculator_1.sub(a, b)}")
-print(f"{a} * {b} = {calculator_1.mul(a, b)}")
-print(f"{a} / {b} = {calculator_1.div(a, b)}")
+    a = 10
+    b = 5
+
+    print(f"{a} + {b} = {calculator_1.add(a, b)}")
+    print(f"{a} - {b} = {calculator_1.sub(a, b)}")
+    print(f"{a} * {b} = {calculator_1.mul(a, b)}")
+    print(f"{a} / {b} = {calculator_1.div(a, b)}")
