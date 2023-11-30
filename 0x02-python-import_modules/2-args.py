d6d53e0b7f7664b2806e70f0db8bac07bb048a5d
wafaa <wafaagaafar@gmail.com>
2023-11-30 03:34:50 -0800

    2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 import sys
 
-def print_arguments():
-    arguments = sys.argv[1:]
+if __name__ == '__main__':
+    def print_arguments():
+        arguments = sys.argv[1:]
     num_arguments = len(arguments)
 
     if num_arguments == 0:
         print("0 arguments.")
     else:
-        print(f"{num_arguments} argument{'s' if num_arguments != 1 else ''}:")
-        for i, arg in enumerate(arguments, start=1):
-            print(f"{i}: {arg}")
+        print("{} argument{}:".format(num_arguments, 's' if num_arguments != 1 else '')
+                for i, arg in enumerate(arguments, start=1):
+                print("{}: {}".format(i, arg))
 
-print_arguments()
+                print_arguments()
