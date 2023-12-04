a0342b8a3ab74cb72d6a34b022bd07df7f50926c
wafaa <wafaagaafar@gmail.com>
2023-12-04 03:57:09 -0800

    2

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def replace_in_list(my_list, idx, element):
+    for i in range(len(my_list)):
+        if idx < 0 or idx > len(my_list):
+            return my_list
+        else:
+            if i == idx:
+                my_list[i] = element
+                return my_list
