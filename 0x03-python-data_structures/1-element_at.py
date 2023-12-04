773348f917b471e181a2e77e18ae92d20a33df3f
wafaa <wafaagaafar@gmail.com>
2023-12-04 03:55:24 -0800

    1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,8 +1,4 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    if idx < 0 or idx > len(my_list):
-        print("None")
-    else:
-        for i in range(len(my_list)):
-            if i == idx:
-                return my_list[i]
+    if idx >= 0 and idx < len(my_list):
+        return my_list[idx]
