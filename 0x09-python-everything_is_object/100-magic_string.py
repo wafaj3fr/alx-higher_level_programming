efe6ef1f3f32524719318959ccb74a4644946908
wafa <wafaagaafar@gmail.com>
2024-01-05 12:54:47 -0800

    100

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,3 +1,4 @@
-def magic_string():
-    magic_string.counter = getattr(magic_string, 'counter', 0) + 1
-    return "BestSchool" + ", BestSchool" * (magic_string.counter - 1)
+#!/usr/bin/python3
+def magic_string(string=[]):
+    string += ["BestSchool"]
+    return ", ".join(string)
