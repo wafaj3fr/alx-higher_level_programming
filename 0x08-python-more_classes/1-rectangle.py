fbb4d5fa9f20c46ba15eb26c3f8bfeca4e1a7fc0
wafa <wafaagaafar@gmail.com>
2024-01-04 16:21:21 -0800

    -

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,16 +1,23 @@
 #!/usr/bin/python3
+"""
+Rectangle class
+"""
 
 class Rectangle:
+    """representation"""
     def __init__(self, width=0, height=0):
+        """initialization"""
         self.width = width
         self.height = height
 
     @property
     def width(self):
+        """getter"""
         return self.__width
 
     @width.setter
     def width(self, value):
+        """setter"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
         if value < 0:
@@ -19,10 +26,12 @@ class Rectangle:
 
     @property
     def height(self):
+        """getter"""
         return self.__height
 
     @height.setter
     def height(self, value):
+        """setter"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
         if value < 0:
