289c4fcd5110ba7f2cbc89ccfd963f4648913ab5
wafa <wafaagaafar@gmail.com>
2024-01-08 14:53:49 -0800

    0

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -10,15 +10,3 @@ def lookup(obj):
     result = [name for name in names if callable(getattr(obj, name))]
 
     return result
-
-class MyClass1(object):
-    pass
-
-class MyClass2(object):
-    my_attr1 = 3
-    def my_meth(self):
-        pass
-
-print(lookup(MyClass1))
-print(lookup(MyClass2))
-print(lookup(int))
