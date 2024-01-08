a4f2343fe9818c49362fb2d92a33409c63875f07
wafa <wafaagaafar@gmail.com>
2024-01-08 15:24:33 -0800

    3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""is_kind_of_class function"""
+
+
+def is_kind_of_class(obj, a_class):
+    """Method returns True if an object is an instance of a class"""
+
+    return isinstance(obj, a_class)
