36530210a05b7f0da2c3201a266113ecbfebef1b
wafa <wafaagaafar@gmail.com>
2024-01-08 15:13:48 -0800

    2

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -4,5 +4,4 @@
 
 class MyList(list):
     def print_sorted(self):
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(self))
---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+"""is_same_class function"""
+
+
+def is_same_class(obj, a_class):
+    return type(obj) is a_class
