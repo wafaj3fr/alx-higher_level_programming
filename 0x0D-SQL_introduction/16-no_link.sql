c2bd3f28fb23c04fb7df0e6a9a798aca1b1fcf50
wafa <wafaagaafar@gmail.com>
2024-02-13 11:36:03 -0800

    16

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,5 +1,5 @@
 -- lists all records of the table second_table of the database hbtn_0c_0.
-SELECT score, name
-FROM hbtn_0c_0.second_table
-WHERE name IS NOT NULL
-ORDER BY score DESC;
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC;
