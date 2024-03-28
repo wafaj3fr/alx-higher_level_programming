211ac62aaa522e7879aefb3c9c2516ce6e3d4c7d
wafa <wafaagaafar@gmail.com>
2024-03-28 07:28:23 -0700

    0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+This script fetches https://alx-intranet.hbtn.io/status using the urllib package.
+"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(url) as response:
+        body = response.read().decode('utf-8')
+        print("- Body response:")
+        print("\t- type:", type(body))
+        print("\t- content:", body)
+
