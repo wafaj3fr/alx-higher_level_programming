a7ceea2d401e914ef9e8732c8d1daff9fa12c643
wafa <wafaagaafar@gmail.com>
2024-04-03 19:33:41 -0700

    1

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,8 +1,23 @@
 #!/usr/bin/python3
 
 class Base():
+    """
+    Base class for managing id attributes in other classes.
+
+    Attributes:
+        __nb_objects (int): Private class attribute to keep track of the number of objects created.
+        id (int): Public instance attribute representing the unique identifier of an instance.
+
+    """
     __nb_objects = 0
     def __init__(self, id=None):
+        """
+        Initialize a Base instance.
+
+        Args:
+            id (int, optional): The unique identifier for the instance. Defaults to None.
+                If id is not provided, it will be automatically generated.
+        """
         if id is not None:
             self.id = id
         else:
