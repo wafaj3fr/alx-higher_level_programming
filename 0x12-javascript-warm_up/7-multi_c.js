ebb1a3ef75c04d17b70c64a4221c68b22c23be6f
wafa <wafaagaafar@gmail.com>
2024-04-08 18:09:13 -0700

    67

---
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+'use strict';
+
+const arg = process.argv[2];
+const num = parseInt(arg);
+
+if (!isNaN(num)) {
+  for (let i = 0; i < num; i++) {
+    console.log("C is fun");
+  }
+} else {
+  console.log("Missing number of occurrences");
+}
