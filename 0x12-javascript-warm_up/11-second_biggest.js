0f885f417305d26e08b44cf6d498d2162344dd41
wafa <wafaagaafar@gmail.com>
2024-04-08 18:32:30 -0700

    13

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
 
-
 if (process.argv.length <= 3) {
   console.log(0);
 } else {
---
+++ 0x12-javascript-warm_up/13-add.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+
+'use strict';
+
+function add(a, b) {
+  return a + b;
+}
+
+module.exports = add;
