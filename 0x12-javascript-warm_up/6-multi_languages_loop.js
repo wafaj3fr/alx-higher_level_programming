da4389599ad8f99c4f8826eae4011084be0feb2a
wafa <wafaagaafar@gmail.com>
2024-04-08 18:04:20 -0700

    6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+'use strict';
+
+const lines = [
+  "C is fun",
+  "Python is cool",
+  "JavaScript is amazing"
+];
+
+for (let i = 0; i < lines.length; i++) {
+  console.log(lines[i]);
+}
