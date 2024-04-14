64cf5006ff0bbb3dda4a67ea4ce5495addf9bac9
wafa <wafaagaafar@gmail.com>
2024-04-13 22:49:12 -0700

    5

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,53 @@
+#!/usr/bin/python3
+"""
+Script to list all cities of a given state from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    # Check if all four arguments are provided
+    if len(sys.argv) != 5:
+        print("Usage: {} username password database state_name".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Get MySQL connection parameters and state name from command line arguments
+    username, password, database, state_name = sys.argv[1:5]
+
+    try:
+        # Connect to MySQL server running on localhost at port 3306
+        connection = MySQLdb.connect(host="localhost", port=3306,
+                                     user=username, passwd=password,
+                                     db=database)
+
+        # Create a cursor object to execute SQL queries
+        cursor = connection.cursor()
+
+        # Construct SQL query to select cities of the given state and execute it
+        query = """
+                SELECT cities.id, cities.name
+                FROM cities
+                INNER JOIN states ON cities.state_id = states.id
+                WHERE states.name = %s
+                ORDER BY cities.id ASC
+                """
+        cursor.execute(query, (state_name,))
+
+        # Fetch all rows from the result set
+        rows = cursor.fetchall()
+
+        # Display results
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        print("MySQLdb Error:", e)
+        sys.exit(1)
+
+    finally:
+        # Close cursor and connection
+        if cursor:
+            cursor.close()
+        if connection:
+            connection.close()
