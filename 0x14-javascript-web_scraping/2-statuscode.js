fdad98d449eee5cef1db82e882e31d31a5012992
wafa <wafaagaafar@gmail.com>
2024-05-21 03:12:59 -0700

    5

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -3,7 +3,7 @@
 const request = require('request')
 const url = process.argv[2]
 
-request(url, function(err, response, bosy) {
+request(url, function (err, response, body) {
 	if (err) {
 		console.log(err)
 	} else {
